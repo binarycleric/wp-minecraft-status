4c55dc029b74027fa020b047de1963bee202b38f
Jon Daniel <binarycleric@gmail.com>
2012-02-04 23:34:47 -0500

    css tweaks

--- javascript/minecraft.js
+++ javascript/minecraft.js
@@ -2,7 +2,6 @@ jQuery(document).ready(function() {
   var data = {
     action: 'server_status'
   };
-
   if(jQuery("#minecraft-server-status").length) {
     jQuery.post('/wp-admin/admin-ajax.php', data, function(response) {
       jQuery("#minecraft-server-status").html(response);
