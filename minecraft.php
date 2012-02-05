9b7711bb26444c5d608cd92971dfeb69b3b9f8b2
Jon Daniel <binarycleric@gmail.com>
2012-02-04 22:56:30 -0500

    added logos

--- minecraft.php
+++ minecraft.php
@@ -11,6 +11,8 @@ License: None yet (IE, you can't use it)
 
 define('SETTINGS_GROUP', 'minecraft-settings-group');
 
+
+
 add_action('wp_ajax_server_status', 'server_status_callback');
 
 wp_enqueue_script('minecraft', 
@@ -21,16 +23,21 @@ wp_enqueue_script('minecraft',
 
 function server_status_callback() {
   $mc_host="minecraft.pittco.org"; 
-  $mc_port="25565";
+  $mc_port=25565;
+
+
+  $mc_logo = plugins_url("images/minecraft-logo.png", __FILE__);
 
   $socket = @fsockopen($mc_host, $mc_port, $num, $error, 3);
 
   if($socket) {
     $color = "green";
-    $status = "Online";
+    $status = "online";
+    $bg_image = plugins_url("images/minecraft-creeper.png", __FILE__);
   } else {
     $color = "red";
-    $status = "Offline";
+    $status = "offline";
+    $bg_image = plugins_url("images/minecraft-skeleton.png", __FILE__);
   }
 
   @fclose($socket);
